9b974582511bc44b1b0c4c9b0f1446cc15a2bb10
TobinCataldo <tobincataldo@gmail.com>
2014-06-06 15:49:07 -0500

    spelling

--- project2/plot1.R
+++ project2/plot1.R
@@ -1,5 +1,5 @@
 ##
-## The Quesion
+## The Question
 ## Have total emissions from PM2.5 decreased in the United States from 
 ## 1999 to 2008? Using the base plotting system, make a plot showing 
 ## the total PM2.5 emission from all sources for each of the years 

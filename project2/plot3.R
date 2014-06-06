d5567851437cf3f53a31b9eafb3dc950d04269fa
TobinCataldo <tobincataldo@gmail.com>
2014-06-06 14:30:42 -0500

    changed ggplot references

--- project2/plot3.R
+++ project2/plot3.R
@@ -1,5 +1,5 @@
 ##
-## The Quesion
+## The Question
 ## Of the four types of sources indicated by the type (point, nonpoint, 
 ## onroad, nonroad) variable, which of these four sources have seen 
 ## decreases in emissions from 1999–2008 for Baltimore City? Which have 
@@ -20,21 +20,22 @@ plot3 <- function(printToScreen=F, useExistingFile=T,useExistingVar=T) {
     # aggregate the data by year (simple sum)
     trimmed <- subset(NEI, fips=="24510")
     aggdata <- aggregate(trimmed$Emissions, by=list(trimmed$type, trimmed$year), FUN=sum)
-
-    
+    aggdata[1] <- as.factor(aggdata[[1]])
+    aggdata[2] <- as.factor(aggdata[[2]])
+     
     # local variables in AES fail
     # http://stackoverflow.com/questions/10659133/local-variables-within-aes    
     .e <- environment()
     pl <- ggplot(data=aggdata, 
-                 aes(x=factor(aggdata[[2]]),          # X axis data, factors become tick marks
-                     y=round(aggdata[[3]],2),         # scaled Y axis data
-                     label=round(aggdata[[3]],2),
-                     group=aggdata[[1]],
-                     color=aggdata[[1]]
+                 aes(x=Group.2,          # X axis data, factors become tick marks
+                     y=round(x,2),         # scaled Y axis data
+                     label=round(x,2),
+                     group=Group.1,
+                     color=Group.1
                      ),
                  environment =.e) +  # adds point value to graph
         geom_point() +     
-        geom_line(size=1.1) +          # connect the points
+        geom_line(size=.6) +          # connect the points
         geom_text(size=3, hjust = 1, vjust = 1.25) +     # writes the labels    
         ylab("Total PM2.5 Emissions") + 
         xlab("Year") + 
@@ -42,10 +43,13 @@ plot3 <- function(printToScreen=F, useExistingFile=T,useExistingVar=T) {
         ggtitle("PM2.5 Emissions By Year and Type for Baltimore City, Maryland") +       
         theme(plot.title = element_text(size=10, face="bold")) +
         theme(legend.position=c(.88,.84)) +    
-        theme(legend.title = element_text(size=7, face="bold")) +
-        theme(legend.text = element_text(size=6)) +
+        theme(legend.title = element_text(size=8, face="bold")) +
+        theme(legend.text = element_text(size=8)) +
+        theme(legend.background = element_rect(fill = "transparent", 
+                                               color = "transparent", 
+                                               size=0)) +
         theme(axis.title = element_text(size=8)) +
-        theme(axis.text = element_text(size=7)) 
+        theme(axis.text = element_text(size=8)) 
 
       
     if (printToScreen) {
